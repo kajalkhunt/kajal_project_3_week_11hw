e49094ad51721636ebf547b629bb418e1fcd9b88
kajalkhunt <kajaljkhunt@gmail.com>
2022-11-15 20:34:43 +0000

    kajal-project-3-week-11hw

---
+++ src/com_herokuapp/FirefoxBrowserTest.java
@@ -0,0 +1,53 @@
+package com_herokuapp;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class FirefoxBrowserTest {
+
+        public static void main(String[] args) {
+            String baseUrl = "http://the-internet.herokuapp.com/login";
+            System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+            WebDriver driver = new FirefoxDriver();
+            //Launch the URL
+            driver.get(baseUrl);
+            //Maximise window
+            driver.manage().window().maximize();
+            //We give implicity time to driver
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+            //Get the title of the page
+            String title = driver.getTitle();
+            System.out.println("Page title is :" + title);
+
+            //get Current URL
+            System.out.println("Current URL :" + driver.getCurrentUrl());
+
+            //Get Page Source
+            System.out.println("Page source:" + driver.getPageSource());
+
+            //Find the email field element and send the email
+            WebElement emailField = driver.findElement(By.id("username"));
+            //Type email to email field
+            emailField.sendKeys("ABC123@gmail.com");
+
+            // Find the password field element
+            WebElement passwordfield = driver.findElement(By.name("password"));
+            passwordfield.sendKeys("test123");
+
+
+            //Close the browser
+            driver.quit();
+
+        }
+
+    }
+
+
+
+
+
